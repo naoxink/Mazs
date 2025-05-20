93bc2dac68673ab0bbe4a415d29e556ca2edf956
J. Gabriel Corpas <naoxink@icloud.com>
2025-05-20 11:45:31 +0000

    Añadidas infos de strikes

--- strikes.js
+++ strikes.js
@@ -3,12 +3,52 @@ const Strikes = {
     location: 'Ojo del norte',
     alias: [ 'shiver' ]
   },
+  5207: {
+    location: 'Ojo del norte',
+    alias: [ 'Kodans' ]
+  },
+  5210: {
+    location: 'Ojo del norte',
+    alias: [ 'WoJ', 'Whisper' ]
+  },
+  5194: {
+    location: 'Ojo del norte',
+    alias: [ 'BS' ]
+  },
+  5328: {
+    location: 'Ojo del norte',
+    alias: [ 'CW' ]
+  },
+  5233: {
+    location: 'Ojo del norte',
+    alias: [ 'Fraenir' ]
+  },
   6353: {
     location: 'Piedra arbórea',
     alias: [ 'AH' ]
   },
+  6457: {
+    location: 'Piedra arbórea',
+    alias: [ 'XJJ' ]
+  },
+  6327: {
+    location: 'Piedra arbórea',
+    alias: [ 'KO' ]
+  },
+  6307: {
+    location: 'Piedra arbórea',
+    alias: [ 'HT' ]
+  },
+  6803: {
+    location: 'Piedra arbórea',
+    alias: [ 'OLC' ]
+  },
   7130: {
     location: 'Torre del brujo',
     alias: [ 'CO' ]
+  },
+  7154: {
+    location: 'Torre del brujo',
+    alias: [ 'ToF', 'Febe' ]
   }
 }
