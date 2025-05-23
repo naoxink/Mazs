fd0468bfb589e24e96dba9e41f6e1d04b861a308
J. Gabriel Corpas <naoxink@icloud.com>
2025-05-23 10:56:30 +0000

    Corregido index guardado de tier

--- Mazs.js
+++ Mazs.js
@@ -414,7 +414,7 @@
 			tier.classList[isDone ? 'remove' : 'add']('done')
 		})
 		if(index > 0){
-			mazs.saveTierDone(fractalID, index)
+			mazs.saveTierDone(fractalID, index + 1)
 		}else{
 			localStorage.removeItem('fractal-tier-done-' + fractalID)
 		}
